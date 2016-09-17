5636ff2aaadb45f526a6001e9e6542dbf5092567
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2016-09-16 23:55:54 -0400

    Timestamp the object for debugging

--- TextUpdate.java
+++ TextUpdate.java
@@ -1,12 +1,14 @@
 // Serialized class that can be sent across a TCP socket
 
 import java.io.*;
+import java.util.Date;
 
 class TextUpdate implements Serializable
 {
 	private String text;
 	private int start;
 	private int end;
+	private long time;		// For debug
 
 	public String text()
 	{
@@ -28,10 +30,11 @@ class TextUpdate implements Serializable
 		text = t;
 		start = s;
 		end = e;
+		time = System.currentTimeMillis();
 	}
 
 	public String toString()
 	{
-		return "(" + start + ") " + text + " (" + end + ")";
+		return time%1000 + ": (" + start + ") " + text + " (" + end + ")";
 	}
 }
